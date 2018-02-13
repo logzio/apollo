dd924b2f3a39befa8003fd60c40f1b5a344befed
ofer-velich <ofer@logz.io>
2018-02-13 22:41:15 +0200

    adding travis integration

---
+++ ui/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -ev
+
+cd ui
+    docker run --rm -v $PWD:/data digitallyseamless/nodejs-bower-grunt npm install
+    docker run --rm -v $PWD:/data digitallyseamless/nodejs-bower-grunt bower install
+    docker run --rm -v $PWD:/data digitallyseamless/nodejs-bower-grunt grunt build --force
+cd -
+
