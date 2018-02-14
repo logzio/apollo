d5ee76b63f2e7d7aeb418f4821c64af0f22fd12f
ofer-velich <ofer@logz.io>
2018-02-14 11:21:13 +0200

    reducing output

--- ui/build.sh
+++ ui/build.sh
@@ -3,8 +3,8 @@
 set -ev
 
 cd ui
-    docker run --rm -v $PWD:/data digitallyseamless/nodejs-bower-grunt npm install
-    docker run --rm -v $PWD:/data digitallyseamless/nodejs-bower-grunt bower install
+    docker run --rm -v $PWD:/data digitallyseamless/nodejs-bower-grunt npm install > /dev/null
+    docker run --rm -v $PWD:/data digitallyseamless/nodejs-bower-grunt bower install > /dev/null
     docker run --rm -v $PWD:/data digitallyseamless/nodejs-bower-grunt grunt build --force
 cd -
 
