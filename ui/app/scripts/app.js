d26db0d5a0fb5bab9f85ee559060282985c70cc8
Shahar Gotshtat <ShaharGotshtat@users.noreply.github.com>
2018-08-29 10:56:20 +0300

    Groups UI (#124)
    * Groups UI
    
    * Fix success message to something reasonable

--- ui/app/scripts/app.js
+++ ui/app/scripts/app.js
@@ -315,6 +315,74 @@ angular
                       }
                     }
     });
+
+    $stateProvider
+               .state('group', {
+                 url:'/group',
+                 templateUrl: 'views/group/main.html',
+                 resolve: {
+                     loadMyDirectives:function($ocLazyLoad){
+                         return $ocLazyLoad.load(
+                         {
+                             name:'apollo',
+                             files:[
+                             'scripts/directives/header/header.js',
+                             'scripts/directives/header/header-notification/header-notification.js',
+                             'scripts/directives/sidebar/sidebar.js',
+                             'scripts/directives/sidebar/sidebar-search/sidebar-search.js'
+                             ]
+                         }),
+                         $ocLazyLoad.load(
+                         {
+                            name:'toggle-switch',
+                            files:["bower_components/angular-toggle-switch/angular-toggle-switch.min.js",
+                                   "bower_components/angular-toggle-switch/angular-toggle-switch.css"
+                               ]
+                         }),
+                         $ocLazyLoad.load(
+                         {
+                           name:'ngAnimate',
+                           files:['bower_components/angular-animate/angular-animate.js']
+                         }),
+                         $ocLazyLoad.load(
+                         {
+                           name:'ngCookies',
+                           files:['bower_components/angular-cookies/angular-cookies.js']
+                         }),
+                         $ocLazyLoad.load(
+                         {
+                           name:'ngResource',
+                           files:['bower_components/angular-resource/angular-resource.js']
+                         }),
+                         $ocLazyLoad.load(
+                         {
+                           name:'ngSanitize',
+                           files:['bower_components/angular-sanitize/angular-sanitize.js']
+                         }),
+                         $ocLazyLoad.load(
+                         {
+                           name:'ngTouch',
+                           files:['bower_components/angular-touch/angular-touch.js']
+                         })
+                     }
+                 }
+             })
+          .state('group.configure',{
+                templateUrl:'views/group/configure.html',
+                controller: 'groupsCtrl',
+                url:'/configure',
+                resolve: {
+                          loadMyFiles:function($ocLazyLoad) {
+                            return $ocLazyLoad.load({
+                              name:'apollo',
+                              files:[
+                              'scripts/services/apolloApiService.js',
+                              'scripts/controllers/groupsCtrl.js'
+                              ]
+                            })
+                          }
+                        }
+        });
     $stateProvider
                .state('auth', {
                  url:'/auth',
