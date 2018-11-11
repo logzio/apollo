293a721a218e6c69b0c7cc7175566eafa6038639
mayhuberman <may.huberman@logz.io>
2018-11-11 15:19:12 +0200

    changed method name to updateCommitDate

--- apollo-backend/src/main/java/io/logz/apollo/dao/DeployableVersionDao.java
+++ apollo-backend/src/main/java/io/logz/apollo/dao/DeployableVersionDao.java
@@ -2,7 +2,7 @@ package io.logz.apollo.dao;
 
 import io.logz.apollo.models.DeployableVersion;
 import org.apache.ibatis.annotations.Param;
-
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -17,4 +17,5 @@ public interface DeployableVersionDao {
     DeployableVersion getLatestDeployableVersionByServiceId(@Param("serviceId") int serviceId);
     void addDeployableVersion(DeployableVersion deployableVersion);
     List<DeployableVersion> getDeployableVersionForMultiServices(@Param("serviceIdsCsv") String serviceIdsCsv, @Param("numOfServices") int numOfServices);
+    void updateCommitDate(@Param("id") int id, @Param("commitDate") Date commitDate);
 }
