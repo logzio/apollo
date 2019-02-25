03efb2e73705d8ec2fc4a7b2340435dd71a15d0a
ShaharGotshtat <shahar.g@logz.io>
2019-02-25 17:04:03 +0200

    Alignment and remove redundant print

--- ui/app/scripts/controllers/signupCtrl.js
+++ ui/app/scripts/controllers/signupCtrl.js
@@ -6,29 +6,31 @@ angular.module('apollo')
             function (apolloApiService, $scope, $timeout, $state, growl, usSpinnerService) {
 
             apolloApiService.getAllDeploymentRoles().then(
-            function(response) {
-                $scope.roles = response.data;
-            });
+                function(response) {
+                    $scope.roles = response.data;
+                }
+            );
 
             $scope.signup = function() {
                 apolloApiService.signup($scope.userEmail, $scope.firstName, $scope.lastName, $scope.password).then(
-                function(response){
-                    addRoleToUser($scope.userEmail, $scope.role.id);
-                },
-                function(error){
-                    growl.error("Got from apollo API: " + error.status + " (" + error.statusText + ")", {ttl: 7000});
-                })
+                    function(response){
+                        addRoleToUser($scope.userEmail, $scope.role.id);
+                    },
+                    function(error){
+                        growl.error("Got from apollo API: " + error.status + " (" + error.statusText + ")", {ttl: 7000});
+                    }
+                )
             }
 
             var addRoleToUser = function(userEmail, roleId) {
                 apolloApiService.addUserToDeploymentRole(userEmail, roleId).then(
-                function(response) {
-                    growl.success("Successfully created new user!", {ttl:7000});
-                    $scope.buttonDisabled = true;
-                },
-                function(error) {
-                    console.log("error!");
-                    growl.error("Got from apollo API: " + error.status + " (" + error.statusText + ")", {ttl: 7000});
-                });
+                    function(response) {
+                        growl.success("Successfully created new user!", {ttl:7000});
+                        $scope.buttonDisabled = true;
+                    },
+                    function(error) {
+                        growl.error("Got from apollo API: " + error.status + " (" + error.statusText + ")", {ttl: 7000});
+                    }
+                );
             }
 }]);
