cbcb15d8d89e41e5cdf6072edb1d4900766cc2fa
ShaharGotshtat <shahar.g@logz.io>
2019-02-25 14:23:39 +0200

    Choose role when adding a new user

--- ui/app/scripts/controllers/signupCtrl.js
+++ ui/app/scripts/controllers/signupCtrl.js
@@ -5,14 +5,30 @@ angular.module('apollo')
                                     '$timeout' , '$state', 'growl', 'usSpinnerService',
             function (apolloApiService, $scope, $timeout, $state, growl, usSpinnerService) {
 
+            apolloApiService.getAllDeploymentRoles().then(
+            function(response) {
+                $scope.roles = response.data;
+            });
+
             $scope.signup = function() {
                 apolloApiService.signup($scope.userEmail, $scope.firstName, $scope.lastName, $scope.password).then(
                 function(response){
-                    growl.success("Successfully created new user!", {ttl:7000});
-                    $scope.buttonDisabled = true;
+                    addRoleToUser($scope.userEmail, $scope.role.id);
                 },
                 function(error){
                     growl.error("Got from apollo API: " + error.status + " (" + error.statusText + ")", {ttl: 7000});
                 })
+            }
+
+            var addRoleToUser = function(userEmail, roleId) {
+                apolloApiService.addUserToDeploymentRole(userEmail, roleId).then(
+                function(response) {
+                    growl.success("Successfully created new user!", {ttl:7000});
+                    $scope.buttonDisabled = true;
+                },
+                function(error) {
+                    console.log("error!");
+                    growl.error("Got from apollo API: " + error.status + " (" + error.statusText + ")", {ttl: 7000});
+                });
             }
 }]);
