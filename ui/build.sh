df7b32245442e4716ea6dbd75de3a6894a3d10b9
ifatregev <ifat.regev@logz.io>
2019-12-16 16:37:04 +0200

    Nginx conf

--- ui/build.sh
+++ ui/build.sh
@@ -7,3 +7,4 @@ cd ui
     docker run --rm -v $PWD:/data digitallyseamless/nodejs-bower-grunt bower install &> /dev/null
     docker run --rm -v $PWD:/data digitallyseamless/nodejs-bower-grunt grunt build --force
 cd -
+
