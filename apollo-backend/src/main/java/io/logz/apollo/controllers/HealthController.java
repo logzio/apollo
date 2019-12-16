4cd784773cc1dec403fd31a63839a3c0bf839177
mayhuberman <may.huberman@logz.io>
2019-12-16 16:34:59 +0200

    Adding logs about unhealthy environment

--- apollo-backend/src/main/java/io/logz/apollo/controllers/HealthController.java
+++ apollo-backend/src/main/java/io/logz/apollo/controllers/HealthController.java
@@ -2,10 +2,13 @@ package io.logz.apollo.controllers;
 
 import com.google.inject.Inject;
 import io.logz.apollo.common.HttpStatus;
+import io.logz.apollo.dao.EnvironmentDao;
 import io.logz.apollo.kubernetes.KubernetesHealth;
 import org.rapidoid.annotation.Controller;
 import org.rapidoid.annotation.GET;
 import org.rapidoid.http.Req;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
@@ -19,16 +22,21 @@ import static java.util.Objects.requireNonNull;
 public class HealthController {
 
     private final KubernetesHealth kubernetesHealth;
+    private final EnvironmentDao environmentDao;
+    private static final Logger logger = LoggerFactory.getLogger(HealthController.class);
 
     @Inject
-    public HealthController(KubernetesHealth kubernetesHealth) {
+    public HealthController(KubernetesHealth kubernetesHealth, EnvironmentDao environmentDao) {
         this.kubernetesHealth= requireNonNull(kubernetesHealth);
+        this.environmentDao= requireNonNull(environmentDao);
     }
 
     @GET("/health")
     public void getHealth(Req req) {
         Map<Integer, Boolean> environmentsHealthMap = kubernetesHealth.getEnvironmentsHealthMap();
         if (environmentsHealthMap.containsValue(false)) {
+            environmentsHealthMap.keySet().forEach(environmentId ->
+                    logger.error("Unhealthy environment, environmentId: {}, environmentName: {}.", environmentId, environmentDao.getEnvironment(environmentId).getName()));
             assignJsonResponseToReq(req, HttpStatus.INTERNAL_SERVER_ERROR, environmentsHealthMap);
         } else {
             assignJsonResponseToReq(req, HttpStatus.OK, environmentsHealthMap);
