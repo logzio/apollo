06772f21c945751a6751908cdf1badfda58b31fc
Darina Klots <darina@logz.io>
2020-01-22 18:34:29 +0200

    add deployment description

--- apollo-backend/src/main/java/io/logz/apollo/kubernetes/KubernetesHandlerStore.java
+++ apollo-backend/src/main/java/io/logz/apollo/kubernetes/KubernetesHandlerStore.java
@@ -4,8 +4,10 @@ import com.google.common.annotations.VisibleForTesting;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.logz.apollo.dao.DeploymentDao;
 import io.logz.apollo.dao.ServiceDao;
+import io.logz.apollo.models.Deployment;
 import io.logz.apollo.models.Environment;
 import io.logz.apollo.notifications.ApolloNotifications;
+import io.logz.apollo.services.DeploymentService;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -20,22 +22,24 @@ public class KubernetesHandlerStore {
     private final ApolloToKubernetesStore apolloToKubernetesStore;
     private final ApolloNotifications apolloNotifications;
     private final Map<Integer, KubernetesHandler> kubernetesHandlerMap;
+    private final DeploymentService deploymentService;
 
     @Inject
-    public KubernetesHandlerStore(ApolloToKubernetesStore apolloToKubernetesStore, ApolloNotifications apolloNotifications) {
+    public KubernetesHandlerStore(ApolloToKubernetesStore apolloToKubernetesStore, ApolloNotifications apolloNotifications, DeploymentService deploymentService) {
         this.apolloToKubernetesStore = requireNonNull(apolloToKubernetesStore);
         this.apolloNotifications = requireNonNull(apolloNotifications);
         this.kubernetesHandlerMap = new ConcurrentHashMap<>();
+        this.deploymentService = requireNonNull(deploymentService);
     }
 
     public KubernetesHandler getOrCreateKubernetesHandler(Environment environment) {
         return kubernetesHandlerMap.computeIfAbsent(environment.getId(),
-                key -> new KubernetesHandler(apolloToKubernetesStore, environment, apolloNotifications));
+                key -> new KubernetesHandler(apolloToKubernetesStore, environment, apolloNotifications, deploymentService));
     }
 
     @VisibleForTesting
     public KubernetesHandler getOrCreateKubernetesHandlerWithSpecificClient(Environment environment, KubernetesClient kubernetesClient) {
         return kubernetesHandlerMap.computeIfAbsent(environment.getId(),
-                key -> new KubernetesHandler(apolloToKubernetesStore, kubernetesClient, environment, apolloNotifications));
+                key -> new KubernetesHandler(apolloToKubernetesStore, kubernetesClient, environment, apolloNotifications, deploymentService));
     }
 }
