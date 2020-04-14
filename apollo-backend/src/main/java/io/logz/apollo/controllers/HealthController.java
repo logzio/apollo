652fba9391940f505a635233e6eaf7d755ccfeb4
mayhuberman <may.huberman@logz.io>
2020-04-14 14:08:05 +0300

    the HealthController will work only of the slave's scoped environments

--- apollo-backend/src/main/java/io/logz/apollo/controllers/HealthController.java
+++ apollo-backend/src/main/java/io/logz/apollo/controllers/HealthController.java
@@ -4,6 +4,7 @@ import com.google.inject.Inject;
 import io.logz.apollo.common.HttpStatus;
 import io.logz.apollo.dao.EnvironmentDao;
 import io.logz.apollo.kubernetes.KubernetesHealth;
+import io.logz.apollo.services.SlaveService;
 import org.rapidoid.annotation.Controller;
 import org.rapidoid.annotation.GET;
 import org.rapidoid.http.Req;
@@ -11,6 +12,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import java.util.Map;
+import java.util.Set;
+
 import static io.logz.apollo.common.ControllerCommon.assignJsonResponseToReq;
 import static java.util.Objects.requireNonNull;
 
@@ -22,17 +25,25 @@ public class HealthController {
 
     private final KubernetesHealth kubernetesHealth;
     private final EnvironmentDao environmentDao;
+    private final SlaveService slaveService;
     private static final Logger logger = LoggerFactory.getLogger(HealthController.class);
 
     @Inject
-    public HealthController(KubernetesHealth kubernetesHealth, EnvironmentDao environmentDao) {
-        this.kubernetesHealth= requireNonNull(kubernetesHealth);
-        this.environmentDao= requireNonNull(environmentDao);
+    public HealthController(KubernetesHealth kubernetesHealth, EnvironmentDao environmentDao, SlaveService slaveService) {
+        this.kubernetesHealth = requireNonNull(kubernetesHealth);
+        this.environmentDao = requireNonNull(environmentDao);
+        this.slaveService = requireNonNull(slaveService);
     }
 
     @GET("/health")
     public void getHealth(Req req) {
+        Set<Integer> scopedEnvironments = slaveService.getScopedEnvironments();
         Map<Integer, Boolean> environmentsHealthMap = kubernetesHealth.getEnvironmentsHealthMap();
+        environmentsHealthMap.keySet()
+                             .stream()
+                             .filter(envId -> !scopedEnvironments.contains(envId))
+                             .forEach(environmentsHealthMap::remove);
+
         if (environmentsHealthMap.containsValue(false)) {
             environmentsHealthMap.entrySet()
                                  .stream()
