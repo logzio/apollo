e36b8a0563e5d5db47cdca5c4bbdb480ca331eca
KfirShainholtz <kfir.s@logz.io>
2021-01-11 13:03:37 +0200

    Add marker notification test and tests to check filtered notifications

--- apollo-backend/src/test/java/io/logz/apollo/NotificationsTest.java
+++ apollo-backend/src/test/java/io/logz/apollo/NotificationsTest.java
@@ -1,6 +1,7 @@
 package io.logz.apollo;
 
 import io.logz.apollo.clients.ApolloTestClient;
+import io.logz.apollo.exceptions.ApolloClientException;
 import io.logz.apollo.helpers.Common;
 import io.logz.apollo.helpers.ModelsGenerator;
 import io.logz.apollo.helpers.StandaloneApollo;
@@ -22,21 +23,29 @@ import org.mockserver.verify.VerificationTimes;
 
 public class NotificationsTest {
 
-    private static int slackMockPort = Common.getAvailablePort();
-    private static final String pseudoUrl = "/slack/submit";
-    private static String slackMockDomain = "http://localhost:" + slackMockPort + pseudoUrl;
+    private static int mockHttpServerPort = Common.getAvailablePort();
+    private static final String slackPath = "/slack/submit";
+    private static final String markersPath = "/markers/create-marker";
+    private static String slackMockDomain = "http://localhost:" + mockHttpServerPort + slackPath;
+    private static String markersServiceMockDomain = "http://localhost:" + mockHttpServerPort + markersPath;
     private static String slackNotificationConfiguration;
+    private static String markerNotificationConfiguration;
 
     private MockServerClient mockServerClient;
 
     @Rule
-    public MockServerRule mockServerRule = new MockServerRule(this, slackMockPort);
+    public MockServerRule mockServerRule = new MockServerRule(this, mockHttpServerPort);
 
     @BeforeClass
     public static void beforeClass() {
         slackNotificationConfiguration = "{\n" +
                 "        \"channel\": \"#slack\",\n" +
-                "        \"webhookUrl\": \""+ slackMockDomain +"\"\n" +
+                "        \"webhookUrl\": \"" + slackMockDomain + "\"\n" +
+                "}";
+
+        markerNotificationConfiguration = "{\n" +
+                "        \"apiToken\": \"bla\",\n" +
+                "        \"markersEndpointUrl\": \"" + markersServiceMockDomain + "\"\n" +
                 "}";
     }
 
@@ -57,13 +66,76 @@ public class NotificationsTest {
 
         StandaloneApollo.getOrCreateServer().getInstance(ApolloNotifications.class).notify(Deployment.DeploymentStatus.DONE, deployment);
 
-        waitForRequest();
+        waitForRequest(slackPath);
 
         mockServerClient.verify(
                 HttpRequest.request()
-                .withMethod("POST")
-                .withPath(pseudoUrl),
+                        .withMethod("POST")
+                        .withPath(slackPath),
                 VerificationTimes.exactly(1)
+        );
+    }
+
+    @Test
+    public void testMarkerNotification() throws Exception {
+        ApolloTestClient apolloTestClient = Common.signupAndLogin();
+        Notification notification = ModelsGenerator.createAndSubmitNotification(apolloTestClient,
+                NotificationType.MARKER, markerNotificationConfiguration);
+
+        Environment environment = apolloTestClient.getEnvironment(notification.getEnvironmentId());
+        Service service = apolloTestClient.getService(notification.getServiceId());
+
+        DeployableVersion deployableVersion = ModelsGenerator.createAndSubmitDeployableVersion(apolloTestClient, service);
+        Deployment deployment = ModelsGenerator.createAndSubmitDeployment(apolloTestClient, environment, service, deployableVersion);
+
+        mockMarkersService();
+
+        StandaloneApollo.getOrCreateServer().getInstance(ApolloNotifications.class).notify(Deployment.DeploymentStatus.STARTED, deployment);
+
+        waitForRequest(markersPath);
+
+        mockServerClient.verify(
+                HttpRequest.request()
+                        .withMethod("POST")
+                        .withPath(markersPath),
+                VerificationTimes.exactly(1)
+        );
+    }
+
+    @Test
+    public void testNotificationsNotSentWhenShouldBeFiltered() throws Exception {
+        ApolloTestClient apolloTestClient = Common.signupAndLogin();
+
+        Notification markerNotification = ModelsGenerator.createAndSubmitNotification(apolloTestClient,
+                NotificationType.MARKER, markerNotificationConfiguration);
+
+        ModelsGenerator.createAndSubmitNotification(apolloTestClient,
+                NotificationType.SLACK, slackNotificationConfiguration);
+
+        Environment environment = apolloTestClient.getEnvironment(markerNotification.getEnvironmentId());
+        Service service = apolloTestClient.getService(markerNotification.getServiceId());
+
+        DeployableVersion deployableVersion = ModelsGenerator.createAndSubmitDeployableVersion(apolloTestClient, service);
+        Deployment deployment = ModelsGenerator.createAndSubmitDeployment(apolloTestClient, environment, service, deployableVersion);
+
+        mockMarkersService();
+        mockSlackServer();
+
+        StandaloneApollo.getOrCreateServer().getInstance(ApolloNotifications.class).notify(Deployment.DeploymentStatus.PENDING, deployment);
+
+        waitForRequest(markersPath);
+
+        mockServerClient.verifyZeroInteractions();
+
+        StandaloneApollo.getOrCreateServer().getInstance(ApolloNotifications.class).notify(Deployment.DeploymentStatus.STARTED, deployment);
+
+        waitForReq uest(slackPath);
+
+        mockServerClient.verify(
+                HttpRequest.request()
+                        .withMethod("POST")
+                        .withPath(slackPath),
+                VerificationTimes.exactly(0)
         );
     }
 
@@ -71,19 +143,31 @@ public class NotificationsTest {
 
         mockServerClient.when(
                 HttpRequest.request()
-                .withMethod("POST")
-                .withPath(pseudoUrl)
+                        .withMethod("POST")
+                        .withPath(slackPath)
         ).respond(
                 HttpResponse.response()
-                .withStatusCode(200)
+                        .withStatusCode(200)
         );
     }
 
-    private void waitForRequest() throws InterruptedException {
+    private void mockMarkersService() {
+
+        mockServerClient.when(
+                HttpRequest.request()
+                        .withMethod("POST")
+                        .withPath(markersPath)
+        ).respond(
+                HttpResponse.response()
+                        .withStatusCode(200)
+        );
+    }
+
+    private void waitForRequest(String requestPath) throws InterruptedException {
         long waitDuration = 100;
         while (mockServerClient.retrieveRecordedRequests(HttpRequest.request()
                 .withMethod("POST")
-                .withPath(pseudoUrl)
+                .withPath(requestPath)
         ).length == 0 && waitDuration-- > 0) {
             Thread.sleep(10);
         }
